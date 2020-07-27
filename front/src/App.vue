29919b54afeea51ae3db9134b49c388aa313c5b0
toshiye <sasakikenji@gmail.com>
2020-07-27 17:53:50 -0300

    sasaki20200727

---
+++ front/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <index/>
+    <SideBar/>
+  </div>
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld.vue'
+import Index from './components/main/Index.vue'
+import SideBar from './components/SideBar/SideBar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    //HelloWorld
+    Index,
+    SideBar
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ front/src/components/main/Index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div id="main">
+        <h1>Teste index!!!</h1>
+    </div>    
+</template>
+
+<script src="./Index.js"></script>
+<style>
+    #main{
+        text-align: center;
+    }
+</style>
